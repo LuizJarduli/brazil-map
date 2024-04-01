bdb3bf758607e47aae099a95d1e86db5c9fb44c9
Luiz Miguel <luizjarduli@gmail.com>
2024-04-01 00:43:09 -0300

    feat: render initial POC, success

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,19 @@
 import { defineConfig } from 'vite';
 
-export default defineConfig({});
+export default defineConfig({
+  server: {
+    port: 3000,
+  },
+  build: {
+    lib: {
+      entry: 'src/index.ts',
+      formats: ['es'],
+      name: 'BrazilMap',
+    },
+    rollupOptions: {
+      onwarn(warning) {
+        console.warn(`${warning.message}`);
+      },
+    },
+  },
+});
