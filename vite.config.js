901b1b0c222a849849ccbea9a8ee00b17b86f5d4
Luiz Miguel <luizjarduli@gmail.com>
2024-04-03 00:00:17 -0300

    build: package minification setup

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,7 @@
+import resolve from '@rollup/plugin-node-resolve';
+import terser from '@rollup/plugin-terser';
+import minifyHTML from 'rollup-plugin-minify-html-literals';
+import summary from 'rollup-plugin-summary';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
@@ -11,6 +15,25 @@ export default defineConfig({
       name: 'BrazilMap',
     },
     rollupOptions: {
+      output: {
+        entryFileNames: 'brazil-mentioned.js',
+        plugins: [
+          // Minify JS
+          terser({
+            ecma: 2021,
+            module: true,
+            warnings: true,
+          }),
+        ],
+      },
+      plugins: [
+        // Resolve bare module specifiers to relative paths
+        resolve(),
+        // Minify HTML template literals
+        minifyHTML.default(),
+        // Print bundle summary
+        summary(),
+      ],
       onwarn(warning) {
         console.warn(`${warning.message}`);
       },
